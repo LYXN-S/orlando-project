101c760738234807c58582141b7d68587ffd35ea
Daniel Victorioso <danielvictorioso03@gmail.com>
2026-03-03 12:07:42 +0800

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/orlandoprestige/orlandoproject/OrlandoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.orlandoprestige.orlandoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrlandoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrlandoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orlandoprestige/orlandoproject/OrlandoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlandoprestige.orlandoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrlandoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
