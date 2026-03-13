5da707b08a48e2aafb3402a2f7810a25f0db5a77
Daniel Victorioso <danielvictorioso03@gmail.com>
2026-03-13 09:18:24 +0800

    Add MovementType enum to define inventory movement types

---
+++ src/main/java/com/orlandoprestige/orlandoproject/inventory/internal/domain/MovementType.java
@@ -0,0 +1,9 @@
+package com.orlandoprestige.orlandoproject.inventory.internal.domain;
+
+public enum MovementType {
+    STOCK_IN,
+    STOCK_OUT,
+    ADJUSTMENT,
+    ORDER_DEDUCTION,
+    ORDER_REVERSAL
+}
